92d13f6823210d97d4369be4b54da1e71b6f216c
dgsousa <sousa.danielg@gmail.com>
2017-03-12 10:19:54 -0700

    generate rooms on canvas

--- app/index.jsx
+++ app/index.jsx
@@ -4,25 +4,38 @@ import './scss/application.scss';
 
 
 
-class Square extends React.Component {
+
+
+
+
+
+class Room extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			style: {
-				top: this.props.row * 10,
-				left: this.props.column * 10,
-				background: 'white'
-			}
+			style: {}
 		}
+	}
 
+	componentWillMount() {
+		this.generateDimensions();
+	}
+
+	generateDimensions() {
+		let style = {
+			top: Math.floor(Math.random() * 500),
+			left: Math.floor(Math.random() * 1000),
+			width: Math.floor(Math.random() * 100 + 50),
+			height: Math.floor(Math.random() * 100 + 50)
+		}
+		this.state.style = style;
+		this.setState(this.state);	
 	}
 
 	render() {
-		
-		return (
-			<div
-				style={this.state.style}
-				className={"square"}>
+		return(
+			<div className="room"
+				 style={this.state.style}>
 			</div>
 		)
 	}
@@ -32,36 +45,19 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
-
 	constructor(props) {
 		super(props)
 	}
 
-
 	render() {
-		let style = {
-			width: this.props.width * 10,
-			height: this.props.height * 10,
-			border: "1px solid blue"
+		let rooms = [];
+		for(let i = 0; i < 5; i++) {
+			rooms.push(<Room key={i}></Room>)
 		}
 		
-		let squares = this.props.board.map(function(square, i) {	
-		let row = this.props.board[i][0];
-		let column = this.props.board[i][1];
-
 		return (
-			
-			<Square 
-				className={"square"}
-				key={i}
-				row={row}
-				column={column}/>
-			)
-		}.bind(this))
-
-		return (
-			<div style={style}>
-				{squares}
+			<div className="board">	
+				{rooms}
 			</div>
 		)
 	}
@@ -70,66 +66,22 @@ class Board extends React.Component {
 
 
 
-class App extends React.Component {
-	
+
+
+class App extends React.Component {	
 	constructor(props) {
 		super(props)
-		this.state = {
-			width: this.props.width,
-			height: this.props.height,
-			board: []
-		}
 	}
-
 	
-	componentWillMount() {
-		this.createBoard();
-		//window.addEventListener('keydown', this.changeCoords.bind(this))
-	}
-
-	// changeCoords(e) {
-	// 	if(e.keyCode === 37 && this.state.coords.x >= 1) {
-	// 		this.state.coords.x -= 1;
-	// 	} else if(e.keyCode === 38 && this.state.coords.y >= 1) {
-	// 		this.state.coords.y -= 1;
-	// 		this.state.style.top += 20;
-	// 	} else if(e.keyCode === 39 && this.state.coords.x <= (this.props.width - 2)) {
-	// 		this.state.coords.x += 1;
-	// 	} else if(e.keyCode === 40 && this.state.coords.y <= (this.props.height - 2)) {
-	// 		this.state.coords.y += 1;
-	// 		this.state.style.top -= 20;
-	// 	}
-	// 	this.setState({
-	// 		coords: this.state.coords,
-	// 		style: this.state.style
-	// 	})
-	// }
-
-	createBoard() {
-		let board = [];
-		let area = this.props.height * this.props.width;
-		for(let i = 0; i < area; i++) {
-			let row = Math.floor(i / this.props.height)
-			let column = i % this.props.height;
-			board.push([row, column]);
-		}
-		this.state.board = board;
-		this.setState(this.state);
-	}
 	
 	render() {
-
 		return (
-			<Board 
-				board={this.state.board}
-				width={this.props.width}
-				height={this.props.height}/>
+			<Board>
+			</Board>
 		)
 	}
-
 }
 
-
-ReactDOM.render(<App width={100} height={100}/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
