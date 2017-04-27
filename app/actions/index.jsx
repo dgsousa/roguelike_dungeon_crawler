87db4083dbebdb13c06cfb1f828bc80b944fb5cf
dgsousa <sousa.danielg@gmail.com>
2017-04-27 12:41:49 -0700

    delete gibberish

--- app/actions/index.jsx
+++ app/actions/index.jsx
@@ -37,12 +37,7 @@ const fight = (entities, message, gameEnd, occupiedSquares) => ({
 	occupiedSquares
 });
 
-const whateber = (THING) => {
-	return (dispatch, getState) => {
-		getState.thingThatIneed + 5
-		dispatch(fight(state.entities))
-	}
-}
+
 
 
 
